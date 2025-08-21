139412df3d4f16a806451a6047d5c71347bd047c
settraxyz <saulgoodman1316@gmail.com>
2025-08-21 12:11:11 +0800

    minor update

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
+import Script from "next/script";
 import { Analytics } from "@vercel/analytics/next";
 
 const geistSans = Geist({
@@ -107,6 +107,20 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Script id="tawk-to" strategy="afterInteractive">
+        {`
+            var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+            (function(){
+              var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+              s1.async=true;
+              s1.src='https://embed.tawk.to/68a6961ed8c4f1192819bc2d/1j35bkqeu';
+              s1.charset='UTF-8';
+              s1.setAttribute('crossorigin','*');
+              s0.parentNode.insertBefore(s1,s0);
+            })();
+          `}
+      </Script>
+
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
